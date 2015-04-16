257cfeceb00123d2138c9e75f0fdf622160f19d5
Mark K Cowan <mark@battlesnake.co.uk>
2015-04-17 01:48:21 +0300

    Linter

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jshintReporter = require('jshint-stylish-source');
+
+var mod = process.env.module || '**';
+
+gulp.task('lint', function () {
+	return gulp.src(['src/' + mod + '/*.js', '!node_modules/**', '!bower_components/**'])
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter))
+		;
+});
