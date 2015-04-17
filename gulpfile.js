1fcb51035077991125b18459c2b2fa3ef5f7e5c0
Mark K Cowan <mark@battlesnake.co.uk>
2015-04-17 12:00:22 +0300

    Linter fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var jshintReporter = require('jshint-stylish-source');
 var mod = process.env.module || '**';
 
 gulp.task('lint', function () {
-	return gulp.src(['src/' + mod + '/*.js', '!node_modules/**', '!bower_components/**'])
+	return gulp.src(['src/' + mod + '/*.js', '!**/node_modules/**', '!**/bower_components/**'])
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter))
 		;
